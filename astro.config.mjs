525b43f1de09a3c3aabb3c443fe47818e5994cf7
Jacob Kim <jpoly1219@gmail.com>
2024-11-26 00:47:26 -0500

    trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://chatlsp.org',
-  site: "https://hazel.org/chatlsp-site.github.io",
+  site: "https://hazel.org/chatlsp-site.github.io/",
 });
