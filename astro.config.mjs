24be9e7ca2962b0067449cbafd89bfda0d605267
Jacob Kim <jpoly1219@gmail.com>
2024-11-26 00:41:38 -0500

    change site to github.io domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chatlsp.org',
-})
+  // site: 'https://chatlsp.org',
+  site: "https://hazel.org/chatlsp-site.github.io",
+});
